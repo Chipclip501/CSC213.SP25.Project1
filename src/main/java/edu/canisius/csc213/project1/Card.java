f11cbc85326019dac0adadae7e7e13de38226470
chipclip501 <chipclip501@gmail.com>
2025-03-11 15:44:02 -0400

    Card class fleshed out

--- src/main/java/edu/canisius/csc213/project1/Card.java
+++ src/main/java/edu/canisius/csc213/project1/Card.java
@@ -12,12 +12,39 @@ public class Card {
     }
 
     // TODO: Define private fields for suit and rank.
-
+    private final Suit suit;
+    private final Rank rank;
     // TODO: Implement the constructor.
-
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
     // TODO: Implement getters for suit and rank.
-
+    public Suit getSuit() {
+        return suit;
+    }
+    public Rank getRank() {
+        return rank;
+    }
     // TODO: Override toString() to return a readable format.
-
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
     // TODO: Override equals() and hashCode() for comparisons.
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Card card = (Card) obj;
+        return suit == card.suit && rank == card.rank;
+    }
+    @Override
+    public int hashCode() {
+        return suit.hashCode() + rank.hashCode();
+    }
 }
